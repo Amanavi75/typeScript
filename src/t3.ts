a29e0744d35e0d0974d6fad797cf44dfb8a9562a
amanavi75 <amanavi75@gmail.com>
2024-07-30 16:29:21 +0530

    function parameter type and their return type

--- src/t3.ts
+++ src/t3.ts
@@ -1,4 +1,20 @@
-function addTwo(num){
-    return num+2;
+// function addTwo(num){
+//     return num+2;
+// }
+// console.log(addTwo(4))
+
+// whenever it is not mentioned , the type declared to any variable is "any"
+
+
+function addtwo(num:number):number{
+    return num+2 
 }
-console.log(addTwo(4))
+
+console.log(addtwo(8))
+//* correct method to write the function
+
+
+
+
+
+
