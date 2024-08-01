6e21166b9d83587224b9730598900d7721016292
amanavi75 <amanavi75@gmail.com>
2024-08-01 13:18:01 +0530

    different ways to create array

--- src/t6.ts
+++ src/t6.ts
@@ -1,3 +1,11 @@
 const price:number []  = [1,2,3,4,4,];
+//*first method 
 
 console.log(price);
+
+
+let price2:(number | string) [] = [1,2,"aman",8]; // second method 
+
+
+let prices:Array <Number> = [1,2,3,4];
+// third method 
