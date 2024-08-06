031fc39fbb74e682d1f23352cdfe4ed37a6555c3
amanavi75 <amanavi75@gmail.com>
2024-08-06 11:02:47 +0530

    methods using the interface with parameter

--- src/t10.ts
+++ src/t10.ts
@@ -1,13 +1,20 @@
 interface User8 {
     email:string,
     id:number,
-    startTrial:()=>string
+    startTrial:()=>string 
+    //startTrail():string ---2nd method 
+
+    getCoupon(couponName:string):number
 }
 
 const hitesh:User8 = {
     email: "aman75@gmail.com", id: 6,
     startTrial :()=>{
         return "starting up the trail"
+    },
+
+    getCoupon:()=>{
+        return 10
     }
 }
 
