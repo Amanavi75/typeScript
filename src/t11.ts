5a7ad9722f96c8ccc46e476dc7195a43580b2d1e
amanavi75 <amanavi75@gmail.com>
2024-08-06 11:46:50 +0530

    done with classes and object

---
+++ src/t11.ts
@@ -0,0 +1,16 @@
+//class 
+class User9{
+    email:string
+    name:string
+    city: string =""
+
+    constructor (email:string,name:string){
+        this.email = email;
+        this.name=name 
+    }
+}
+
+
+// object 
+const hitesh1 = new User9("h@mail.com","hitesh")
+hitesh1.city='patna'
