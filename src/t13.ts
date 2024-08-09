5385c5f9a0511ff38f8042cecf75e74572a0c69c
amanavi75 <amanavi75@gmail.com>
2024-08-09 19:15:21 +0530

    inheritance in typescript

--- src/t13.ts
+++ src/t13.ts
@@ -3,3 +3,37 @@ interface takePhoto{
     filter:string,
     burst:number
 }
+
+class Instagram implements takePhoto{
+    constructor (
+        public cameraMode:string,
+        public filter :string,
+        public burst:number
+
+    ){
+
+    }
+
+
+}
+
+interface story {
+    createStory():void
+}
+
+class Youtube implements takePhoto,story{
+    constructor (
+        public cameraMode:string,
+        public filter :string,
+        public burst:number,
+        public record:number,
+        
+
+    ){
+
+    }
+
+    createStory():void {
+        console.log("story added ")
+    }
+}
