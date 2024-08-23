a91f46c9b9f4c9c4e04ab839cfe03ec038753b0e
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2024-08-23 03:56:14 +0000

    done with abstract class and its method

--- src/t15.ts
+++ src/t15.ts
@@ -1,4 +1,30 @@
-interface user {
-  name:string,
-  id:number
+abstract class takePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+  ) {}
+
+  abstract getTree(): void  
+  // we don't have to provide the defination for this function 
+
+  getTme():number{
+    return 10 ;
+  }
+}
+
+const hite= new insta("aman","adi",5)
+// we can't directly create object from the abstact class
+
+class Insta extends takePhoto {
+  constructor(
+    public cameramode: string,
+    public filter: string,
+    public free :number 
+    
+  ){
+    super(cameramode,filter)
+  }
+  getTree(): void {
+    console.log("called the abstract class method ");
+  }
 }
